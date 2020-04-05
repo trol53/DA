26df6ec132ff4966c08ca76483238aaf34fc87f4
trol53 <nek.savrik@yandex.ru>
2020-04-06 01:21:25 +0300

    create class

---
+++ Calculator/BigInt.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+class BigInteger {
+    public: 
+
+    BigInteger(std::string &value){
+        
+    }
+
+    void Input(){
+        char c;
+        long long buff = 0, check = 0, tmp = 0;
+        while (c = getchar() != '\n'){
+            if (check == 5){
+                buff 
+            }
+        }   
+    }
+
+    private:
+    
+    long long base_number_system = 1000000;
+    std::vector<long long> values;
+};
+
+int main(){
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    
+}
